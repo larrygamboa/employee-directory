a51a0272e490792805e2162cb424481466d0d293
Larry Gamboa <techmedia.lg@gmail.com>
2021-02-22 22:21:32 -0800

    set up TableData component

--- react-employee-directory/src/components/TableData/index.js
+++ react-employee-directory/src/components/TableData/index.js
@@ -4,7 +4,24 @@ import "./index.css";
 function TableData() {
     return ( 
         <div className="container">
-        <p>The table data will go here!</p>
+        <table>
+            <thead>
+                <th>&nbsp;</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>DOB</th>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Image</td>
+                <td>Lorem Ipsum</td>
+                <td>123-456-7890</td>
+                <td>email@email.com</td>
+                <td>01/01/2021</td>
+            </tr>
+        </tbody>
+        </table>
         </div> 
     );
 }
