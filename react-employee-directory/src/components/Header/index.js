bf68420e0033fe0dec7c05b8830cb323150fc449
Larry Gamboa <techmedia.lg@gmail.com>
2021-02-22 21:25:35 -0800

    create Header component

---
+++ react-employee-directory/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return ( 
+    <div>
+      <div className="jumbotron jumbotron-fluid" id="header">
+          <div className="container">
+              <h1 className="display-4">The Bomb Burger Employee Directory</h1>
+              <p className="lead">Use the search to narrow your results. Click on each heading to filter.</p>
+          </div>
+      </div>
+    </div> 
+  );
+}
+
+export default Header;
