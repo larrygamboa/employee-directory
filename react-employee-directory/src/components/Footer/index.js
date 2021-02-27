67157da9ca9718bebab24168d5298148c19d3812
Larry Gamboa <techmedia.lg@gmail.com>
2021-02-26 21:10:18 -0800

    updating the Footer component

--- react-employee-directory/src/components/Footer/index.js
+++ react-employee-directory/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 function Footer() {
   return ( 
     <footer className="container">
-        <p className="text-center">Developed in 2021 by <a href="https://github.com/larrygamboa" target="_blank">Larry Gamboa</a></p>
+        <p className="text-center">Developed using <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a> and <a href="https://reactjs.org/" target="_blank">React</a> by <a href="https://github.com/larrygamboa" target="_blank">Larry Gamboa</a></p>
     </footer>
   );
 }
