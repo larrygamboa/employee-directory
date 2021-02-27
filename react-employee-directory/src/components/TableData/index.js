70ac995b79ce4ada33b7c74ad3d4d045d00765cb
Larry Gamboa <techmedia.lg@gmail.com>
2021-02-26 20:14:27 -0800

    updating TableData and table styles

--- react-employee-directory/src/components/TableData/index.js
+++ react-employee-directory/src/components/TableData/index.js
@@ -6,21 +6,21 @@ function TableData(props) {
     return (
         <div className="row">
             <div className="container">
-            <table>
-                <thead>
-                    <th>&nbsp;</th>
-                    <th onClick={props.handleSort}>Name <i className="fa fa-sort-down"></i></th>
-                    <th>Phone</th>
-                    <th>Email</th>
-                    <th>DOB</th>
+            <table className="table">
+                <thead className="thead-dark">
+                    <th scope="col">&nbsp;</th>
+                    <th scope="col" onClick={props.handleSort}>Name <i className="fa fa-sort-down"></i></th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">DOB</th>
                 </thead>
                 <tbody>
                     {props.employees.map(employee => (
                     <tr key={employee.login.uuid}>
-                        <td><img className="img-fluid" src={employee.picture.medium} /></td>
+                        <td><img alt={"profile image for " + employee.name.first + " " + employee.name.last} className="img-fluid" src={employee.picture.medium} /></td>
                         <td>{employee.name.first} {employee.name.last}</td>
                         <td>{employee.phone}</td>
-                        <td>{employee.email}</td>
+                        <td><a href={"mailto:" + employee.email} target="__blank">{employee.email}</a></td>
                         <td>{employee.dob.date}</td>
                     </tr>
                     ))}
